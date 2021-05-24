01bdad98bf0889930e48d9302380d479de3d7298
hyunmin0317 <choihm9903@naver.com>
2021-05-24 21:49:39 +0900

    hyunmin week06

--- hyunmin/week06/homework/app/src/main/java/com/example/myapplication/Post.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication
-
-import java.io.Serializable
-
-class Post(
-    val owner : String? = null,
-    var content : String? = null,
-    var image : String? = null
-):Serializable
--- hyunmin/week06/homework/app/src/main/java/com/example/myapplication/Song.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication
-
-import java.io.Serializable
-
-class Song(
-    val title : String? = null,
-    val thumbnail : String? = null,
-    val song : String? = null
-):Serializable
